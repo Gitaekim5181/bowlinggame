47383b4c03d553a3c15e19a89ea25fa57ee0c062
DTOL <mirage5181@naver.com>
2020-04-16 15:26:59 +0900

    확인

--- bolwing_Game/bolwing_Game/Form1.Designer.cs
+++ bolwing_Game/bolwing_Game/Form1.Designer.cs
@@ -28,27 +28,282 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.grid_1 = new System.Windows.Forms.DataGridView();
+            this.col0 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col1_1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col2_2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col3_3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col4_4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col5_5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col6_6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col7_7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col8_8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col9_9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col10_10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_Roll = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.Bnt_Start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid_1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button2
+            // splitContainer1
             // 
-            this.button2.Location = new System.Drawing.Point(346, 230);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.grid_1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btn_Roll);
+            this.splitContainer1.Panel2.Controls.Add(this.txt1);
+            this.splitContainer1.Panel2.Controls.Add(this.Bnt_Start);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 571;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // grid_1
+            // 
+            this.grid_1.AllowUserToAddRows = false;
+            this.grid_1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid_1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col0,
+            this.col1,
+            this.col1_1,
+            this.col2,
+            this.col2_2,
+            this.col3,
+            this.col3_3,
+            this.col4,
+            this.col4_4,
+            this.col5,
+            this.col5_5,
+            this.col6,
+            this.col6_6,
+            this.col7,
+            this.col7_7,
+            this.col8,
+            this.col8_8,
+            this.col9,
+            this.col9_9,
+            this.col10,
+            this.col10_10});
+            this.grid_1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid_1.Location = new System.Drawing.Point(0, 0);
+            this.grid_1.Name = "grid_1";
+            this.grid_1.RowHeadersWidth = 51;
+            this.grid_1.RowTemplate.Height = 27;
+            this.grid_1.Size = new System.Drawing.Size(571, 450);
+            this.grid_1.TabIndex = 0;
+            // 
+            // col0
+            // 
+            this.col0.HeaderText = "";
+            this.col0.MinimumWidth = 6;
+            this.col0.Name = "col0";
+            this.col0.Width = 125;
+            // 
+            // col1
+            // 
+            this.col1.HeaderText = "1번게임";
+            this.col1.MinimumWidth = 6;
+            this.col1.Name = "col1";
+            this.col1.Width = 125;
+            // 
+            // col1_1
+            // 
+            this.col1_1.HeaderText = "";
+            this.col1_1.MinimumWidth = 6;
+            this.col1_1.Name = "col1_1";
+            this.col1_1.Width = 125;
+            // 
+            // col2
+            // 
+            this.col2.HeaderText = "2번게임";
+            this.col2.MinimumWidth = 6;
+            this.col2.Name = "col2";
+            this.col2.Width = 125;
+            // 
+            // col2_2
+            // 
+            this.col2_2.HeaderText = "";
+            this.col2_2.MinimumWidth = 6;
+            this.col2_2.Name = "col2_2";
+            this.col2_2.Width = 125;
+            // 
+            // col3
+            // 
+            this.col3.HeaderText = "3번게임";
+            this.col3.MinimumWidth = 6;
+            this.col3.Name = "col3";
+            this.col3.Width = 125;
+            // 
+            // col3_3
+            // 
+            this.col3_3.HeaderText = "";
+            this.col3_3.MinimumWidth = 6;
+            this.col3_3.Name = "col3_3";
+            this.col3_3.Width = 125;
+            // 
+            // col4
+            // 
+            this.col4.HeaderText = "4번게임";
+            this.col4.MinimumWidth = 6;
+            this.col4.Name = "col4";
+            this.col4.Width = 125;
+            // 
+            // col4_4
+            // 
+            this.col4_4.HeaderText = "";
+            this.col4_4.MinimumWidth = 6;
+            this.col4_4.Name = "col4_4";
+            this.col4_4.Width = 125;
+            // 
+            // col5
+            // 
+            this.col5.HeaderText = "5번게임";
+            this.col5.MinimumWidth = 6;
+            this.col5.Name = "col5";
+            this.col5.Width = 125;
+            // 
+            // col5_5
+            // 
+            this.col5_5.HeaderText = "";
+            this.col5_5.MinimumWidth = 6;
+            this.col5_5.Name = "col5_5";
+            this.col5_5.Width = 125;
+            // 
+            // col6
+            // 
+            this.col6.HeaderText = "6번게임";
+            this.col6.MinimumWidth = 6;
+            this.col6.Name = "col6";
+            this.col6.Width = 125;
+            // 
+            // col6_6
+            // 
+            this.col6_6.HeaderText = "";
+            this.col6_6.MinimumWidth = 6;
+            this.col6_6.Name = "col6_6";
+            this.col6_6.Width = 125;
+            // 
+            // col7
+            // 
+            this.col7.HeaderText = "7번게임";
+            this.col7.MinimumWidth = 6;
+            this.col7.Name = "col7";
+            this.col7.Width = 125;
+            // 
+            // col7_7
+            // 
+            this.col7_7.HeaderText = "";
+            this.col7_7.MinimumWidth = 6;
+            this.col7_7.Name = "col7_7";
+            this.col7_7.Width = 125;
+            // 
+            // col8
+            // 
+            this.col8.HeaderText = "8번게임";
+            this.col8.MinimumWidth = 6;
+            this.col8.Name = "col8";
+            this.col8.Width = 125;
+            // 
+            // col8_8
+            // 
+            this.col8_8.HeaderText = "";
+            this.col8_8.MinimumWidth = 6;
+            this.col8_8.Name = "col8_8";
+            this.col8_8.Width = 125;
+            // 
+            // col9
+            // 
+            this.col9.HeaderText = "9번게임";
+            this.col9.MinimumWidth = 6;
+            this.col9.Name = "col9";
+            this.col9.Width = 125;
+            // 
+            // col9_9
+            // 
+            this.col9_9.HeaderText = "";
+            this.col9_9.MinimumWidth = 6;
+            this.col9_9.Name = "col9_9";
+            this.col9_9.Width = 125;
+            // 
+            // col10
+            // 
+            this.col10.HeaderText = "10번게임";
+            this.col10.MinimumWidth = 6;
+            this.col10.Name = "col10";
+            this.col10.Width = 125;
+            // 
+            // col10_10
+            // 
+            this.col10_10.HeaderText = "";
+            this.col10_10.MinimumWidth = 6;
+            this.col10_10.Name = "col10_10";
+            this.col10_10.Width = 125;
+            // 
+            // btn_Roll
+            // 
+            this.btn_Roll.Location = new System.Drawing.Point(32, 53);
+            this.btn_Roll.Name = "btn_Roll";
+            this.btn_Roll.Size = new System.Drawing.Size(75, 23);
+            this.btn_Roll.TabIndex = 2;
+            this.btn_Roll.Text = "Roll";
+            this.btn_Roll.UseVisualStyleBackColor = true;
+            this.btn_Roll.Click += new System.EventHandler(this.btn_Roll_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(32, 10);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 25);
+            this.txt1.TabIndex = 1;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            this.txt1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt1_KeyPress);
+            // 
+            // Bnt_Start
+            // 
+            this.Bnt_Start.Location = new System.Drawing.Point(138, 12);
+            this.Bnt_Start.Name = "Bnt_Start";
+            this.Bnt_Start.Size = new System.Drawing.Size(75, 23);
+            this.Bnt_Start.TabIndex = 0;
+            this.Bnt_Start.Text = "시작";
+            this.Bnt_Start.UseVisualStyleBackColor = true;
+            this.Bnt_Start.Click += new System.EventHandler(this.Bnt_Start_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid_1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,7 +311,32 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView grid_1;
+        private System.Windows.Forms.Button btn_Roll;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button Bnt_Start;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col0;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col1_1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col2_2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col3_3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col4_4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col5_5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col6_6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col7_7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col8_8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col9_9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col10_10;
     }
 }
 
