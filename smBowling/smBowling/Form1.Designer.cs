d705df4ab436f088953ce1c9cf0d7d5ee73e8d7d
tomato9 <tomato896@naver.com>
2020-04-21 18:15:21 +0900

    점수

--- smBowling/smBowling/Form1.Designer.cs
+++ smBowling/smBowling/Form1.Designer.cs
@@ -34,11 +34,17 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -50,6 +56,9 @@
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView4);
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView3);
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView2);
             this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
             // 
             // splitContainer1.Panel2
@@ -58,38 +67,37 @@
             this.splitContainer1.Panel2.Controls.Add(this.button2);
             this.splitContainer1.Panel2.Controls.Add(this.button1);
             this.splitContainer1.Panel2.Controls.Add(this.textBox1);
-            this.splitContainer1.Size = new System.Drawing.Size(1057, 564);
-            this.splitContainer1.SplitterDistance = 830;
-            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.Size = new System.Drawing.Size(873, 583);
+            this.splitContainer1.SplitterDistance = 685;
             this.splitContainer1.TabIndex = 0;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(11, 9);
+            this.dataGridView1.Location = new System.Drawing.Point(10, 7);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 27;
-            this.dataGridView1.Size = new System.Drawing.Size(807, 542);
+            this.dataGridView1.Size = new System.Drawing.Size(546, 144);
             this.dataGridView1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 18);
+            this.label1.Location = new System.Drawing.Point(12, 14);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 15);
+            this.label1.Size = new System.Drawing.Size(61, 12);
             this.label1.TabIndex = 3;
             this.label1.Text = "참여인원 :";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(13, 72);
+            this.button2.Location = new System.Drawing.Point(11, 58);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(102, 51);
+            this.button2.Size = new System.Drawing.Size(89, 41);
             this.button2.TabIndex = 2;
             this.button2.Text = "ROLL";
             this.button2.UseVisualStyleBackColor = true;
@@ -97,10 +105,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(143, 9);
+            this.button1.Location = new System.Drawing.Point(118, 7);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 41);
+            this.button1.Size = new System.Drawing.Size(66, 33);
             this.button1.TabIndex = 1;
             this.button1.Text = "시작";
             this.button1.UseVisualStyleBackColor = true;
@@ -108,17 +116,44 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(90, 12);
+            this.textBox1.Location = new System.Drawing.Point(79, 10);
             this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(35, 25);
+            this.textBox1.Size = new System.Drawing.Size(31, 21);
             this.textBox1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(10, 156);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 23;
+            this.dataGridView2.Size = new System.Drawing.Size(546, 139);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(10, 301);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 23;
+            this.dataGridView3.Size = new System.Drawing.Size(546, 135);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(12, 442);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.RowTemplate.Height = 23;
+            this.dataGridView4.Size = new System.Drawing.Size(546, 127);
+            this.dataGridView4.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1057, 564);
+            this.ClientSize = new System.Drawing.Size(873, 583);
             this.Controls.Add(this.splitContainer1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
@@ -129,6 +164,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -141,6 +179,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
 
--- smBowling/smBowling/Form1.cs
+++ smBowling/smBowling/Form1.cs
@@ -15,6 +15,8 @@ namespace smBowling
         int rollcnt = 1;
         int frameCnt = 0;
         int totalScore = 0;
+        int firstPin = 0;
+        int secondPin = 0;
         public Form1()
         {
             InitializeComponent();
@@ -25,14 +27,14 @@ namespace smBowling
             dataGridView1.Refresh();
             rollcnt = 1;
             frameCnt = 0;
-            if(textBox1.Text == "")
+            if (textBox1.Text == "")
             {
                 MessageBox.Show("참가인원을 입력해주세요");
             }
-            else if (int.Parse(textBox1.Text) > 9)
+            else if (int.Parse(textBox1.Text) > 4)
             {
-                MessageBox.Show("0~8 사이를 입력해주세요.");
-            } 
+                MessageBox.Show("1~4 사이를 입력해주세요.");
+            }
             else
             {
                 DataTable table = new DataTable();
@@ -49,7 +51,7 @@ namespace smBowling
                 table.Columns.Add("10", typeof(string));
                 dataGridView1.DataSource = table;
                 dataGridView1.ReadOnly = true;
-                for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                for (int i = 1; i < dataGridView1.Columns.Count; i++)
                 {
                     DataGridViewColumn column = dataGridView1.Columns[i];
                     column.Width = 40;
@@ -57,97 +59,92 @@ namespace smBowling
                 table.Rows.Add();
                 table.Rows.Add();
                 table.Rows.Add();
+                dataGridView1.Rows[0].Cells[0].Value = "1번레인";
+                dataGridView1.Rows[1].Cells[0].Value = "스페어";
+                dataGridView1.Rows[2].Cells[0].Value = "보너스";
+                dataGridView1.Rows[3].Cells[0].Value = "점수합계";
+                DataGridViewColumn column1 = dataGridView1.Columns[0];
+                column1.Width = 70;
+
             }
         }
 
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Random r = new Random();
-            int firstscore = r.Next(0, 11);
-            int fscore = 0;
-            int secondscore = r.Next(0, 11 - fscore);
-            if (rollcnt < 11)
-            {                  
+            if(rollcnt < 11)
+            {
+                Random r = new Random();
+                int firstscore = r.Next(0, 11);
+                int fscore = 0;
+                int secondscore = r.Next(0, 11 - fscore);
                 if (frameCnt == 0)
-                {                 
-                    
-                    
-                        dataGridView1.Rows[frameCnt].Cells[rollcnt].Value = firstscore;
-                  
-                    
-                    //dataGridView1.Rows[2].Cells[rollcnt].Value = firstscore;
-                    frameCnt++;
-                    if (rollcnt == 10)
-                    {
-                        if (firstscore == 10)
-                        {
-                           secondscore = r.Next(0, 11);
-                           dataGridView1.Rows[frameCnt].Cells[rollcnt].Value = secondscore;
-                        }
-                    }
-                } 
-                else 
                 {
-                    String sscore= dataGridView1.Rows[frameCnt - 1].Cells[rollcnt].Value.ToString();
-                    if(sscore == "10")
-                    {
-                        fscore = 10;
-                        dataGridView1.Rows[frameCnt].Cells[rollcnt].Value = 0;
-                        dataGridView1.Rows[3].Cells[rollcnt].Value = fscore;
-                    }
-                    else
-                    {
-                        fscore = Int32.Parse(sscore);
-                        secondscore = r.Next(0, 11 - fscore);
-                       
-                       
-                            dataGridView1.Rows[frameCnt].Cells[rollcnt].Value = secondscore;
-                            dataGridView1.Rows[3].Cells[rollcnt].Value = fscore + secondscore;
-                       
-                    }
-                   if(rollcnt == 10)
-                    {
-                        if(fscore + secondscore == 10)
-                        {
-                            int bonusScore = r.Next(0, 11);
-                            dataGridView1.Rows[2].Cells[rollcnt].Value = bonusScore;
-                            dataGridView1.Rows[3].Cells[rollcnt].Value = fscore + secondscore + bonusScore;
-                        }
-                        else
-                        {
-                            dataGridView1.Rows[3].Cells[rollcnt].Value = fscore + secondscore;
-                        }
-                    }
-                    if(rollcnt > 1)
+                    dataGridView1.Rows[frameCnt].Cells[rollcnt].Value = firstscore;                    
+                    frameCnt++;
+                   
+                }
+                else
+                {
+                    String sscore = dataGridView1.Rows[frameCnt - 1].Cells[rollcnt].Value.ToString();
+                    fscore = Int32.Parse(sscore);
+                    secondscore = r.Next(0, 11 - fscore);
+
+
+                    dataGridView1.Rows[frameCnt].Cells[rollcnt].Value = secondscore;
+                    dataGridView1.Rows[3].Cells[rollcnt].Value = fscore + secondscore;
+                    if (rollcnt > 1)
                     {
                         string lastScore = dataGridView1.Rows[3].Cells[rollcnt - 1].Value.ToString();
                         totalScore = Int32.Parse(lastScore);
                         dataGridView1.Rows[3].Cells[rollcnt].Value = fscore + secondscore + totalScore;
-                   
+
                         int strikeScore = Int32.Parse(dataGridView1.Rows[0].Cells[rollcnt - 1].Value.ToString());
                         int spareScore = Int32.Parse(dataGridView1.Rows[1].Cells[rollcnt - 1].Value.ToString());
-                        int lasyFrameScore = Int32.Parse(dataGridView1.Rows[3].Cells[rollcnt - 1].Value.ToString());
-                    
+                        int lastFrameScore = Int32.Parse(dataGridView1.Rows[3].Cells[rollcnt - 1].Value.ToString());
+
                         if (strikeScore == 10)
                         {
-                            dataGridView1.Rows[3].Cells[rollcnt - 1].Value = fscore + secondscore + lasyFrameScore;
+                            dataGridView1.Rows[3].Cells[rollcnt - 1].Value = fscore + secondscore + lastFrameScore;
+                            lastScore = dataGridView1.Rows[3].Cells[rollcnt - 1].Value.ToString();
+                            totalScore = Int32.Parse(lastScore);
                             dataGridView1.Rows[3].Cells[rollcnt].Value = fscore + secondscore + totalScore;
                         }
-                        else if(strikeScore != 10 && strikeScore + spareScore == 10)
+                        else if (strikeScore != 10 && strikeScore + spareScore == 10)
                         {
-                            dataGridView1.Rows[3].Cells[rollcnt - 1].Value = fscore + lasyFrameScore;
-                            dataGridView1.Rows[3].Cells[rollcnt].Value = fscore + lasyFrameScore + fscore + secondscore;
+                            dataGridView1.Rows[3].Cells[rollcnt - 1].Value = fscore + lastFrameScore;
+                            dataGridView1.Rows[3].Cells[rollcnt].Value = fscore + lastFrameScore + fscore + secondscore;
                             //dataGridView1.Rows[3].Cells[rollcnt].Value = fscore + secondscore + totalScore;
+                        }
+                    }
+                    if(rollcnt == 10)
+                    {
+                        if(fscore == 10)
+                        {
+                            secondscore = r.Next(0, 11);
+                            dataGridView1.Rows[frameCnt].Cells[rollcnt].Value = secondscore;
+                        }
+                        if (fscore + secondscore == 10)
+                        {
+                            int bonusScore = r.Next(0, 11);
+                            dataGridView1.Rows[2].Cells[rollcnt].Value = bonusScore;
+                            string lastScore = dataGridView1.Rows[3].Cells[rollcnt - 1].Value.ToString();
+                            totalScore = Int32.Parse(lastScore);
+                            dataGridView1.Rows[3].Cells[rollcnt].Value = fscore + secondscore + bonusScore + totalScore;
+                        }
+                        else
+                        {
+                            string lastScore = dataGridView1.Rows[3].Cells[rollcnt - 1].Value.ToString();
+                            totalScore = Int32.Parse(lastScore);
+                            dataGridView1.Rows[3].Cells[rollcnt].Value = fscore + secondscore + totalScore;
                         }
                     }
                     frameCnt--;
                     rollcnt++;
-                    
-                }
 
+                }
             }
-            
+
 
         }
 
@@ -157,7 +154,7 @@ namespace smBowling
             {
                 e.Handled = true;
             }
-            
+
         }
 
     }
