831b5603385964a0e35b028fca6c0be3bc24832c
DTOL <mirage5181@naver.com>
2020-04-29 10:26:17 +0900

    소스정리

--- bolwing_Game/bolwing_Game/Form1.cs
+++ bolwing_Game/bolwing_Game/Form1.cs
@@ -38,9 +38,7 @@ namespace bolwing_Game
         public Form1()
         {
             InitializeComponent();
-
         }
-
         private void txt1_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!(char.IsDigit(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back)))
@@ -52,10 +50,7 @@ namespace bolwing_Game
             {
                 Handled_txt = false;
             }
-
-
         }
-
         private void txt1_TextChanged(object sender, EventArgs e)
         {
           
@@ -68,15 +63,12 @@ namespace bolwing_Game
                     txt1.Text = "1";
                     txt1.Focus();
                 }
-
-
             }
             Restart_Game();
             grid_1.Rows.Clear();
             grid_1.Refresh();
             Handled_txt = true;
         }
-
         private void Bnt_Start_Click(object sender, EventArgs e)
         {
          
@@ -107,17 +99,11 @@ namespace bolwing_Game
                 grid_1.Refresh();
                 clear = 0;
             }
-
-
-
         }
-
         private void btn_Roll_Click(object sender, EventArgs e)
         {
             Random val = new Random();
             Random val_B = new Random();
-
-
 
             if (grid_1.Rows.Count > 0)
             {
@@ -125,7 +111,6 @@ namespace bolwing_Game
                 {
                     game_Boll = 3;
                 }
-
                 if (game == btncount)
                 {
                     c = 10 - a;
@@ -136,8 +121,6 @@ namespace bolwing_Game
                     gcnt = 0;
                     game_Frm++;
                     game_Rows++;
-
-
                 }
                 if (game_Frm < 1)
                 {
@@ -183,7 +166,6 @@ namespace bolwing_Game
                         spare[gcnt]--;
 
                     }
-
                     if (game >= btncount)
                     {
                         if (game_Boll == 1) //(game_Count<1)
@@ -195,7 +177,6 @@ namespace bolwing_Game
                             //a = 5;
                             c = 10 - a;
                             strike_bonus = 10 - a;
-                            //strike_j[gcnt] -= 10;
 
                             grid_1.Rows[gcnt].Cells[fcnt].Value = a;
 
@@ -213,20 +194,14 @@ namespace bolwing_Game
                                     
                                     grid_1.Rows[gcnt + 1].Cells[game_Frm * 2 - 2].Value = result[gcnt];
                                 }
-
                                 else if (spare[gcnt] >= 1)
                                 {
                                     spare_j[gcnt] = 10;
-                                    //grid_1.Rows[gcnt].Cells[fcnt].Value = a + spare_j;
                                     result[gcnt] += a + spare_j[gcnt];
                                     spare_j[gcnt] -= 10;
                                     grid_1.Rows[gcnt + 1].Cells[fcnt - 1].Value = result[gcnt];
                                 }
-
-
-
                             }
-
                             if (a == 10) //스트라이크 일때
                             {
                                 game_Roll[gcnt] += 2;
@@ -235,7 +210,6 @@ namespace bolwing_Game
 
                                 if (strike[gcnt] > 3)
                                 {
-
                                     strike_j[gcnt] -= 10;
 
                                     result[gcnt] += a + strike_j[gcnt];
@@ -249,7 +223,6 @@ namespace bolwing_Game
                                     spare_j[gcnt] -= 10;
                                     grid_1.Rows[gcnt + 1].Cells[fcnt - 1].Value = result[gcnt];
                                 }
-
                                 if (game_Roll[gcnt] == 20 && strike_bonus == 0)
                                 {
                                     game_Roll[gcnt] -= 1;
@@ -266,11 +239,8 @@ namespace bolwing_Game
                                     game_Boll = 1;//game_Count = 0;
                                 }
 
-
-
                             }
                         }
-
                         else if (game_Boll == 2)
                         {
                             if (grid_1.Rows[gcnt].Cells[fcnt].Value.ToString()=="10")
@@ -296,8 +266,6 @@ namespace bolwing_Game
 
                             if (strike[gcnt] >= 1)
                             {
-
-
                                 if (game_Roll[gcnt] == 20 && strike_bonus == 0)
                                 {
                                     grid_1.Rows[gcnt].Cells[fcnt + 1].Value = b;
@@ -323,34 +291,21 @@ namespace bolwing_Game
                                         grid_1.Rows[gcnt + 1].Cells[fcnt - 1].Value = result[gcnt];
                                         strike[gcnt]--;
                                     }
-
-
                                 }
-                                
-
-                                //strike_j[gcnt]+=10;  //마지막프레임 보너스 경기에 10점이 필요함
-                                
+                          
                             }
                             if (bonus == 0 && strike_bonus != 0)
                             {
                                 spare[gcnt] += 2;
                                 spare_j[gcnt] += 10;
                                 
-                                //strike[gcnt] -= 1;
-                                //strike_j[gcnt] -= 10;
                             }
                             if (game_Roll[gcnt] == 20 && (strike_bonus==0 ||bonus == 0))
                             {
                                 game_Boll = 3;
                             }
-
                             else
                             {
-                                //if(bonus != 0 && game_Roll[gcnt] == 20)
-                                //{
-                                //    result[gcnt] += a + b;
-                                //    grid_1.Rows[gcnt + 1].Cells[fcnt + 1].Value = result[gcnt];
-                                //}
                                 
                                 if (grid_1.Rows[gcnt + 1].Cells[fcnt - 1].Value != null && bonus != 0)
                                 {
@@ -361,9 +316,6 @@ namespace bolwing_Game
                                 gcnt += 2;
                                 game_Boll = 1;//game_Count = 0;
                             }
-
-
-
 
                         }
                         else if (game_Boll == 3)
@@ -399,8 +351,6 @@ namespace bolwing_Game
                     }
 
                 }
-
-
                 else
                 {
                     if (strike_bonus == 0)
@@ -410,16 +360,15 @@ namespace bolwing_Game
                             grid_1.Rows[gcnt].Cells[fcnt + 2].Value = result[gcnt];
                             gcnt += 2;
 
-                            MessageBox.Show(gcnt + "번 게임이 완료되었습니다.!");
+                            MessageBox.Show("게임이 완료되었습니다.!");
 
                         }
-
                         else
                         {
                             Reset_Game();
                         }
                     }
-                    else if (bonus != 0)
+                    else if (bonus == 0 || bonus != 0)
                     {
                         if (grid_1.Columns.Count == fcnt + 2)
                         {
@@ -429,59 +378,21 @@ namespace bolwing_Game
                             MessageBox.Show("게임이 완료되었습니다.!");
 
                         }
-
                         else
                         {
                             Reset_Game();
                         }
                     }
-                    else if (bonus == 0)
-                    {
-                        if (grid_1.Columns.Count == fcnt + 2)
-                        {
-                            grid_1.Rows[gcnt + 1].Cells[fcnt + 1].Value = result[gcnt];
-                            gcnt += 2;
-
-                            MessageBox.Show("게임이 완료되었습니다.!");
-
-                        }
-
-                        else
-                        {
-                            Reset_Game();
-                        }
-                            
-                    }
-                    else
-                    {
-                        if (grid_1.Columns.Count == fcnt)
-                        {
-                            grid_1.Rows[gcnt].Cells[fcnt - 1].Value = result[gcnt];
-                            gcnt++;
-
-                            MessageBox.Show(gcnt + "번 게임이 완료되었습니다.!");
-
-                        }
-
-                        else
-                        {
-                            Reset_Game();
-                        }
-                    }
-
-
+                
                 }
 
             }
         }
         private void Reset_Game()
         {
-           
             MessageBox.Show("새로 시작하세요 모든 게임이 완료되었습니다.!");
             txt1.Text = "";
             Restart_Game();
-
-
         }
         private void Restart_Game()
         {
@@ -493,7 +404,6 @@ namespace bolwing_Game
 
                 }
             }
-            
             fcnt = 0;
             btncount = 0;
             game_Frm = 0;
@@ -520,7 +430,6 @@ namespace bolwing_Game
                 spare_j[j] = 0;
             }
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             this.grid_1.Columns.Add(" ", "");
@@ -549,27 +458,15 @@ namespace bolwing_Game
 
             for (int i = 0; i < this.grid_1.ColumnCount; i++)
             {
-
                 this.grid_1.Columns[i].Width = 70;
-
             }
 
-
-
             this.grid_1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
-
             this.grid_1.ColumnHeadersHeight = this.grid_1.ColumnHeadersHeight * 2;
-
             this.grid_1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.BottomCenter;
-
-
-
             this.grid_1.ColumnWidthChanged += grid_1_ColumnWidthChanged;
-
             this.grid_1.Scroll += grid_1_Scroll;
-
             this.grid_1.Paint += grid_1_Paint;
-
             this.grid_1.CellPainting += grid_1_CellPainting;
 
         }
@@ -583,78 +480,34 @@ namespace bolwing_Game
                 e.PaintBackground(cellRectangle, true);
                 e.PaintContent(cellRectangle);
                 e.Handled = true;
-
             }
-
         }
-
         private void grid_1_Paint(object sender, PaintEventArgs e)
         {
             string[] parentHeaderArray = { "1번프레임", "2번프레임", "3번프레임", "4번프레임", "5번프레임", "6번프레임", "7번프레임", "8번프레임", "9번프레임", "10번프레임" };
-
-
-
             for (int i = 1; i < 21;)
             {
-
                 Rectangle cellRectangle = this.grid_1.GetCellDisplayRectangle(i, -1, true);
-
-
-
                 int nextCellWidth = this.grid_1.GetCellDisplayRectangle(i + 1, -1, true).Width;
-
-
-
                 cellRectangle.X += 1;
-
                 cellRectangle.Y += 1;
-
-
-
                 cellRectangle.Width = cellRectangle.Width + nextCellWidth - 2;
-
                 cellRectangle.Height = cellRectangle.Height / 2 - 2;
-
-
-
                 e.Graphics.FillRectangle(new SolidBrush(this.grid_1.ColumnHeadersDefaultCellStyle.BackColor), cellRectangle);
-
-
-
                 StringFormat stringFormat = new StringFormat();
-
-
-
                 stringFormat.Alignment = StringAlignment.Center;
-
                 stringFormat.LineAlignment = StringAlignment.Center;
-
-
-
                 e.Graphics.DrawString
-
                 (
-
                     parentHeaderArray[i / 2],
-
                     this.grid_1.ColumnHeadersDefaultCellStyle.Font,
-
                     new SolidBrush(this.grid_1.ColumnHeadersDefaultCellStyle.ForeColor),
-
                     cellRectangle,
-
                     stringFormat
-
                 );
-
-
-
                 i += 2;
-
             }
-
         }
-
         private void grid_1_Scroll(object sender, ScrollEventArgs e)
         {
             Rectangle parentHeaderRectangle = this.grid_1.DisplayRectangle;
@@ -662,7 +515,6 @@ namespace bolwing_Game
             this.grid_1.Invalidate(parentHeaderRectangle);
 
         }
-
         private void grid_1_ColumnWidthChanged(object sender, DataGridViewColumnEventArgs e)
         {
             Rectangle headerRectangle = this.grid_1.DisplayRectangle;
